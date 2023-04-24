249089d0853b9ddf8efbd93e46941ed1707b2101
hautc11 <hautachi@gmail.com>
2023-04-24 21:52:29 +0700

    [feat] update test case for ReminderListFragmentTest.kt

--- app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
@@ -1,21 +1,24 @@
 package com.udacity.project4.locationreminders.reminderslist
 
-import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.udacity.project4.R
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.junit.Assert.*
+import org.junit.After
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
+import org.koin.test.AutoCloseKoinTest
+import org.koin.test.KoinTest
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
 
@@ -23,9 +26,35 @@ import org.mockito.Mockito.verify
 @ExperimentalCoroutinesApi
 //UI Testing
 @MediumTest
-class ReminderListFragmentTest {
+class ReminderListFragmentTest : KoinTest {
 
-//    TODO: test the navigation of the fragments.
-//    TODO: test the displayed data on the UI.
-//    TODO: add testing for the error messages.
+	private val navController = mock(NavController::class.java)
+
+	// Test the navigation of the fragments.
+	@Test
+	fun testNavigationToSaveFragment() {
+		val scenario = launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+
+		scenario.onFragment {
+			Navigation.setViewNavController(it.view!!, navController)
+		}
+
+		onView(withId(R.id.addReminderFAB)).perform(click())
+
+		verify(navController).navigate(
+			ReminderListFragmentDirections.toSaveReminder()
+		)
+	}
+
+	// Test the displayed data on the UI.
+	@Test
+	fun testDisplayOfNoDataTextView() {
+		val scenario = launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+
+		scenario.onFragment {
+			Navigation.setViewNavController(it.view!!, navController)
+		}
+
+		onView(withId(R.id.noDataTextView)).check(matches(withText("No Data")))
+	}
 }
